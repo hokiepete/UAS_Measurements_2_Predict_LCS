a068740136011eb19e3fb29cdc81287eaa9a4849
Peter <pnolan1986@gmail.com>
2018-10-27 14:49:02 -0400

    updates

--- 7-17/plot_s1_field.py
+++ 7-17/plot_s1_field.py
@@ -32,12 +32,12 @@ F.close()
 
 plt.close('all')
 width = 6
-height = 6
+height = 4
 plt.figure(1,figsize=(width,height))
-gs1 = gridspec.GridSpec(2, 1)
-gs1.update(hspace=0.1)
+#gs1 = gridspec.GridSpec(2, 1)
+#gs1.update(hspace=0.1)
 #plt.subplot(211)
-plt.subplot(gs1[0,0])
+#plt.subplot(gs1[0,0])
 t_step = 12
 time = tt.gmtime(wrf_time[t_step])
 plt.pcolormesh(lon,lat,3600*s1_wrf[t_step,:,:],vmin=-150,vmax=50)
@@ -45,9 +45,13 @@ plt.colorbar(label='hr$^{-1}$')
 plt.scatter(-106.041504,37.7815528075,color='red')
 plt.title('{0:02d}-{1:02d}-{2:02d}, {3:02d}{4:02d} MDT'.format(time[1],time[2],time[0],time[3]-6,time[4]),**titlefont,y=0.98)
 plt.yticks(**tickfont)
-plt.xticks([])#**tickfont)
+plt.xticks(**tickfont)
+plt.savefig('s1_WRF_2018-07-17_{0}.png'.format(t_step), transparent=False, bbox_inches='tight',pad_inches=0.02,dpi=300)
+
+plt.figure(2,figsize=(width,height))
+
 #plt.subplot(212)
-plt.subplot(gs1[1,0])
+#plt.subplot(gs1[1,0])
 t_step = 23
 time = tt.gmtime(wrf_time[t_step])
 plt.pcolormesh(lon,lat,3600*s1_wrf[t_step,:,:],vmin=-150,vmax=50)
@@ -56,4 +60,4 @@ plt.scatter(-106.041504,37.7815528075,color='red')
 plt.title('{0:02d}-{1:02d}-{2:02d}, {3:02d}{4:02d} MDT'.format(time[1],time[2],time[0],time[3]-6,time[4]),**titlefont,y=0.98)
 plt.yticks(**tickfont)
 plt.xticks(**tickfont)
-plt.savefig('s1_WRF_2018-07-17.png'.format(t_step), transparent=False, bbox_inches='tight',pad_inches=0.02,dpi=300)
+plt.savefig('s1_WRF_2018-07-17_{0}.png'.format(t_step), transparent=False, bbox_inches='tight',pad_inches=0.02,dpi=300)
